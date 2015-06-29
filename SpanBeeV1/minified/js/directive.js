c97c534e606952cf409f1c7b36029cd5b71274fb
nagarjunsanji <nagarjun.sanji@gmail.com>
2015-06-29 18:06:38 +0530

    commit

---
+++ SpanBeeV1/minified/js/directive.js
@@ -0,0 +1,1 @@
+angular.module("app.directives",[]).directive("matchContent",[function(){return{require:"ngModel",link:function(scope,elem,attrs,ctrl){var firstPassword="#"+attrs.matchContent;elem.add(firstPassword).on("keyup",function(){scope.$apply(function(){ctrl.$setValidity("matchContent",elem.val()===$(firstPassword).val());});});}};}]);
---
+++ SpanBeeV1/minified/js/service.js
@@ -0,0 +1,1 @@
+app.service("CommonService",function(){this.close=function($scope){$(".modal").modal("hide");};});
