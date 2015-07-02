fed1ece1d498aa422b3c61b3ca1d5b9a8696a152
rajeshbn1989 <rajeshbn6578@gmail.com>
2015-07-02 17:55:18 +0530

    Coomitting newly added Dao's

---
+++ span-bee/src/main/java/com/spanbee/dao/AuthenticationDao.java
@@ -0,0 +1,11 @@
+package com.spanbee.dao;
+
+import com.spanbee.entities.Customer;
+
+public interface AuthenticationDao {
+  
+  public Customer fetchCustomerInfoByEmailId(String emailId);
+  
+  public boolean setCustomerSessionId(String sessionId, String uniqueId);
+
+}
---
+++ span-bee/src/main/java/com/spanbee/dao/CommonDao.java
@@ -0,0 +1,13 @@
+package com.spanbee.dao;
+
+import com.spanbee.entities.Customer;
+
+public interface CommonDao {
+  
+  public Customer addCustomer(Customer customer);
+  
+  public Customer fetchCustomerByEmailId(String emailId);
+  
+  public boolean setCustomerSessionId(String sessionId, String uniqueId);
+
+}
