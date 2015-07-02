f5c1d4ff69f6511f20dabeebf1b5bdefa8ad9270
span-bee <bshivu111@gmail.com>
2015-07-02 17:35:53 +0530

    ufjug

--- span-bee/src/main/java/com/spanbee/utils/AESSecurity.java
+++ span-bee/src/main/java/com/spanbee/utils/AESSecurity.java
@@ -21,7 +21,7 @@ public class AESSecurity {
   private static final byte[] keyValue = 
       new byte[] { 'T', 'h', 'e', 'B', 'e', 's', 't', 'S', 'e', 'c', 'r','e', 't', 'K', 'e', 'y' };
 
-  public static String encrypt(String Data) throws Exception {
+   public static String encrypt(String Data) throws Exception {
        Key key = generateKey();
       Cipher c = Cipher.getInstance(ALGO);
       c.init(Cipher.ENCRYPT_MODE, key);
